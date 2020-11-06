89ddb32cef411e9587a25e9b6b4fab99992633b4
Justin <js830910@gmail.com>
2020-11-06 23:25:56 +0800

    动态规划中可以节省计算的部分要考虑进去，可以再看看这题

--- 643_findMaxAverage.py
+++ 643_findMaxAverage.py
@@ -7,12 +7,25 @@ Created on 2020-11-06 17:39
 """
 
 
+# def findMaxAverage(nums, k):
+#     if not nums or len(nums) < 0:
+#         return None
+#     recode = [-float("inf")] * (len(nums) - k + 1)
+#     for idx in range(len(recode)):
+#         recode[idx] = sum(nums[idx:idx + k])  # 每次都使用sum的算法过慢
+#     return max(recode) / k
+
 def findMaxAverage(nums, k):
     if not nums or len(nums) < 0:
         return None
-    recode = [0] * (len(nums) - k + 1)
-    for idx in range(len(recode)):
-        recode[idx] = sum(nums[idx:idx + k])
+    recode = [-float("inf")] * len(nums)
+    start = 1
+    end = k
+    recode[0] = sum(nums[:k])
+    while end < len(nums):
+        recode[start] = recode[start-1] + nums[end] - nums[start-1]
+        start += 1
+        end += 1
     return max(recode) / k
 
 if __name__ == '__main__':
