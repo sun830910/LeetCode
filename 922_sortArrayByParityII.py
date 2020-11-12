d91eb49a50e57a42a6da30dd1e2f3b248d6b7967
Justin <js830910@gmail.com>
2020-11-12 17:28:26 +0800

    双指针做法，外层套while的时间复杂度猜测为O(N)，但才约55%

---
+++ 922_sortArrayByParityII.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+"""
+Created on 2020-11-12 16:46
+@Author  : Justin Jiang
+@Email   : jw_jiang@pku.edu.com
+"""
+
+
+def sortArrayByParityII(A):
+    even_idx = 0
+    odd_idx = 1
+    while even_idx < len(A) and odd_idx < len(A):
+        if A[even_idx] % 2 != 0 and A[odd_idx] % 2 != 1:
+            A[odd_idx], A[even_idx] = A[even_idx], A[odd_idx]
+        if A[even_idx] % 2 == 0:
+            even_idx += 2
+        if A[odd_idx] % 2 == 1:
+            odd_idx += 2
+    return A
+
+
+if __name__ == '__main__':
+    A = [4, 2, 5, 7]
+    print(sortArrayByParityII(A))
+    A = [0]
+    print(sortArrayByParityII(A))
+    A = [1, 1, 2, 2]
+    print(sortArrayByParityII(A))
+    A = []
+    print(sortArrayByParityII(A))
