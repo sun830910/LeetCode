9b5bc42732636b163cd4ff5ca420c78667649a32
Justin <js830910@gmail.com>
2020-11-13 15:22:28 +0800

    完成了二分的做法，可以到99%

--- sward_53_missingNumber.py
+++ sward_53_missingNumber.py
@@ -7,16 +7,32 @@ Created on 2020-11-13 15:03
 """
 
 
+# 遍历查找，时间复杂度O(N)
+# def missingNumber(nums):
+#     record = set(idx for idx in range(len(nums) + 1))
+#     for num in nums:
+#         if num in record:
+#             record.remove(num)
+#     return record.pop()
+
 def missingNumber(nums):
-    record = set(idx for idx in range(len(nums) + 1))
-    for num in nums:
-        if num in record:
-            record.remove(num)
-    return record.pop()
+    left = 0
+    right = len(nums) - 1
+    while right >= left:
+        mid = (left + right) // 2
+        if nums[mid] != mid:
+            right = mid - 1
+        else:
+            left = mid + 1
+    return left
 
 
 if __name__ == '__main__':
     nums = [0, 1, 3]
     print(missingNumber(nums))
     nums = [0, 1, 2, 3, 4, 5, 6, 7, 9]
+    print(missingNumber(nums))
+    nums = [0, 1, 2]
+    print(missingNumber(nums))
+    nums = [1, 2, 3]
     print(missingNumber(nums))
