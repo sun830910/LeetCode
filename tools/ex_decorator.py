af29b2b4bef2c278d9e08f7f2f3efebe96b913aa
Justin <js830910@gmail.com>
2020-12-04 17:38:37 +0800

    实际工程中的装饰器练习

--- tools/ex_decorator.py
+++ tools/ex_decorator.py
@@ -7,3 +7,44 @@ Created on 12/4/20 9:02 AM
 
 练习装饰器
 """
+
+import logging
+
+
+# 不带参数的装饰器
+def say_hello(function):
+    def wrapper():
+        print("早上好")
+        function()
+        print("晚上好")
+
+    return wrapper
+
+
+@say_hello
+def say_evening():
+    print("中午好")
+
+
+def decorator(function):
+    def inside(l):
+        try:
+            function(l)
+            print("排序结束")
+        except:
+            print("内部数据错误")
+
+    return inside
+
+
+@decorator
+def show(lt):
+    print("列表排序中")
+    lt.sort()
+    print(lt)
+
+
+if __name__ == '__main__':
+    # say_evening()
+    lt = [3213, 21.3, True, '+', 3.21, '-', 3.21, 3.21, 3.21, 3.21, 3213123, 321, 3, 21, 3, 21, 3, 21, 321]
+    show(lt)
