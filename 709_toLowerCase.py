7457a7182c45b26a0a4087cba056169f2f36f923
Justin <js830910@gmail.com>
2020-12-16 18:12:37 +0800

    简单，无需再看，直接调用了api

---
+++ 709_toLowerCase.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+"""
+Created on 12/16/20 5:57 PM
+@Author  : Justin Jiang
+@Email   : jw_jiang@pku.edu.com
+"""
+
+
+def toLowerCase(str):
+    return str.lower()
+
+
+if __name__ == '__main__':
+    str = "Hello"
+    print(toLowerCase(str))
+    str = ""
+    print(toLowerCase(str))
+    str = " "
+    print(toLowerCase(str))
